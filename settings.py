e55b3e112c44a62678a8573a0372360275001528
Emidio Bianco <ebianco@ebianco-macbookpro2.roam.corp.google.com>
2015-06-04 00:22:09 +0100

    Initial commit

---
+++ settings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+
+WEB_CLIENT_ID = '2487637533-q7r9kcv8om5gj26nnopq8jnvl4v8admp.apps.googleusercontent.com'
+# WEB_CLIENT_ID = ''
